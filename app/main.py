eb7c82bb3dc5ca73506af125bc8351c1b06b59f1
SMMousaviSP <smmousavisp@gmail.com>
2022-12-21 04:04:58 +0100

    Add create endpoint to create tiny url with alias

--- app/main.py
+++ app/main.py
@@ -1,7 +1,37 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import requests
+
+from config import TOKEN
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/create", methods=["POST"])
+def create_short_url():
+    # Get the url from the request body
+    url = request.json.get("url")
+    # Get the alias from the request body
+    alias = request.json.get("alias")
+
+    # Create the short url
+    short_url = create_short_url(url, alias)
+
+    # Return the short url
+    return jsonify({"short_url": short_url})
+
+
+def create_short_url(url, alias):
+    # Set the base url
+    BASE_URL = "https://api.tinyurl.com"
+    # Set the header
+    header = {'Authorization': 'Bearer ' + TOKEN}
+    # Set the body
+    body = {
+        "url": url,
+        "domain": "tiny.one",
+        "alias": alias,
+    }
+    # Make the request
+    response = requests.post(BASE_URL+'/create', data=body, headers=header)
+    # Return the short url
+    json = response.json()
+    return json["data"]["tiny_url"]
