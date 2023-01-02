f42aa9ce47b9702b0bc48267c72f3de8714fd09e
SMMousaviSP <smmousavisp@gmail.com>
2023-01-02 15:44:51 +0100

    Add token_validation

--- app/validation.py
+++ app/validation.py
@@ -1,5 +1,9 @@
 import validators
 
+from settings import (
+    TOKEN_LENGTH_RANGE,
+)
+
 
 def url_validation(url, url_name="Url", ignore_protocol=False):
     """ Raises an exception if url is not valid.
@@ -14,3 +18,17 @@ def url_validation(url, url_name="Url", ignore_protocol=False):
     else:
         if not validators.url(url):
             raise ValueError(f"{url_name} is not valid.")
+
+
+def token_validation(token, token_length_range=TOKEN_LENGTH_RANGE):
+    """ Raises an exception if token is not valid.
+    """
+    min_length, max_length = token_length_range
+
+    if not token:
+        raise ValueError("Token is required.")
+
+    if len(token) < min_length:
+        raise ValueError(f"Token must be at least {min_length} characters long.")
+    if len(token) > max_length:
+        raise ValueError(f"Token must be less than {max_length} characters long.")
